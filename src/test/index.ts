8ee672ead432bf945af33dd463d971b63477417f
Max Wilkinson <mawilkin@umich.edu>
2019-07-08 10:20:36 -0400

    Added license header to source code files

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,3 +1,20 @@
+/*
+ *   Copyright (c) 2019 Ford Motor Company
+ *   All rights reserved.
+
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 //
 // PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
 //
@@ -16,8 +33,8 @@ import * as testRunner from 'vscode/lib/testrunner';
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
 // for more info
 testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
+  ui: 'tdd', // the TDD UI is being used in extension.test.ts (suite, test, etc.)
+  useColors: true // colored output from test results
 });
 
 module.exports = testRunner;
